9a9cdca866e3b2e5a192510725230da0879374de
Eric Klein <eric.j.klein@chicagobooth.edu>
2015-10-12 16:25:57 -0500

    Finished coords -> weather

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -15,18 +15,19 @@ class ForecastController < ApplicationController
     # The latitude the user input is in the string @lat.
     # The longitude the user input is in the string @lng.
     # ==========================================================================
+    url = "https://api.forecast.io/forecast/e252d9a70e7c1d9a8e10993041114ee5/#{@lat},#{@lng}"
+    raw_data_from_darksky = open(url).read
+    hash_from_darksky = JSON.parse(raw_data_from_darksky)
 
+    @current_temperature = hash_from_darksky["currently"]["temperature"]
 
+    @current_summary = hash_from_darksky["currently"]["summary"]
 
-    @current_temperature = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = hash_from_darksky["minutely"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_several_hours = hash_from_darksky["hourly"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
-
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = hash_from_darksky["daily"]["summary"]
 
     render("coords_to_weather.html.erb")
   end
